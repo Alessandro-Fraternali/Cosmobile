4661960a074c1bceac1b8c4cbd334762ca723cfe
WanderingCrow-EleventhMountain <FraternaliAlessandro@gmail.com>
2021-01-11 22:40:53 +0100

    -

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+  $('.autoplay').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+  });
+});
