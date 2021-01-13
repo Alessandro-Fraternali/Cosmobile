56666f5bb1c36139dfdf90460227b96da5f73790
WanderingCrow-EleventhMountain <FraternaliAlessandro@gmail.com>
2021-01-13 18:22:32 +0100

    Aggiunto responsive max-width: 415px

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 $(function () {
-  $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    // arrows: false,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-  });
 
-  $('.autoplay').slick({
-    slidesToShow: 8,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-  });
+  function checkPosition() {
+    if (window.matchMedia('(max-width: 415px)').matches) {
+      $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
+
+      $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+      });
+    } else {
+      $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      });
+
+      $('.autoplay').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+      });
+    }
+  }
+
+  checkPosition();
+
 });
